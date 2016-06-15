8f05ddb1074fffa5b0ea23cd799cb92f54aab601
YutaroKikuchi <s1000614@sendai-nct.jp>
2016-06-15 17:27:57 +0900

    テストコードにDriveTask.javaを更新

--- LogSender_EV3/src/testcode/DriveTask.java
+++ LogSender_EV3/src/testcode/DriveTask.java
@@ -1,23 +1,22 @@
-package linetrace;
+package testcode;
 
 import java.util.TimerTask;
 
 import ev3Viewer.LogSender;
-import hardware.Hardware;
+import lejos.hardware.Battery;
 
 public class DriveTask extends TimerTask{
 
 	private int waitcount = 0;
-	private TurnCalc tc;
-	private WheelMotorCtrl wmc;
+	private static EV3Body body;
 	private LogSender ls;
 
 	private static long starttime;
 
-	DriveTask(WheelMotorCtrl wmc,TurnCalc tc, LogSender ls){
-		this.wmc = wmc;
-		this.tc = tc;
+	DriveTask(LogSender ls, EV3Body body){
+		this.body = body;
 		this.ls = ls;
+		this.waitcount = 0;
 
 		starttime = System.nanoTime();
 	}
@@ -25,34 +24,35 @@ public class DriveTask extends TimerTask{
 	@Override
 	public void run() {
 		// TODO 自動生成されたメソッド・スタブ
-
 		tailControl(0);
 
 		float forward = 40.0F;
-		float turn = tc.calcTurn();
+		float turn = 0.0F;
 
-		wmc.setForward(forward);
-		wmc.setTurn(turn);
+		if(body.getBrightness() > 0.2){
+			turn = 50.0F;
+		}else{
+			turn = -50.0F;
+		}
 
 		if(++waitcount > 50){
-			float[] sample = new float[Hardware.redMode.sampleSize()];
-			Hardware.redMode.fetchSample(sample,0);
-			ls.addLog("Bright", (int)(sample[0] * 100), (System.nanoTime()-starttime)/1000000);
+			ls.addLog("Bright", body.getBrightness(), (System.nanoTime()-starttime)/1000000);
 			waitcount = 0;
 		}
 
-		wmc.controlWheel();
+		Balancer.control(forward, turn, body.getGyroValue(),0.0F, body.motorPortL.getTachoCount(), body.motorPortR.getTachoCount(), Battery.getVoltageMilliVolt());
+
 	}
 
 	private static final void tailControl(int angle) {
-        float pwm = (float)(angle - Hardware.motorPortT.getTachoCount()) * 2.5F; // 比例制御
+        float pwm = (float)(angle - body.motorPortT.getTachoCount()) * 2.5F; // 比例制御
         // PWM出力飽和処理
         if (pwm > 60) {
             pwm = 60;
         } else if (pwm < -60) {
             pwm = -60;
         }
-        Hardware.motorPortT.controlMotor((int)pwm, 1);
+        body.motorPortT.controlMotor((int)pwm, 1);
     }
 
 }
